462a73404b28601d176f0bf4b257d854edc12b36
Tracy Steinberg <tracyadelesteinberg@gmail.com>
2017-07-20 20:49:51 +0100

    airport final?

--- specs/airport_spec.rb
+++ specs/airport_spec.rb
@@ -9,21 +9,15 @@ require_relative('../passenger')
 class AirportTest < Minitest::Test
 
   def setup 
-    @flight1 = Flight.new(65, "New York", '')
-    @flight2 = Flight.new(23, "Dublin", '')
-    @flight3 = Flight.new(17, "Aberdeen", '')
-    @flight4 = Flight.new(34, "LHR", '')
+    @flight1 = Flight.new(65, "New York", '', '')
+    @flight2 = Flight.new(23, "Dublin", '', '')
+    @flight3 = Flight.new(17, "Aberdeen", '', '')
+    @flight4 = Flight.new(34, "LHR", '', '')
 
     @flights = [@flight1, @flight2, @flight3]
 
-    
-    @passenger1 = Passenger.new("David")
-    @passenger2 = Passenger.new("Pooh")
-    @passenger3 = Passenger.new("Max")
-
-    @passengers = [@passenger1, @passenger2, @passenger3]
-
-    @airport = Airport.new("Edinburgh", @flights, @passengers)
+  
+    @airport = Airport.new("Edinburgh", @flights)
   end
 
   def test_airport_has_name
@@ -32,16 +26,6 @@ class AirportTest < Minitest::Test
 
   def test_airport_has_flights
       assert_equal(3, @airport.number_of_flights())
-  end
-
-  def test_can_check_in_passenger
-    @airport.check_in_passenger(@passenger3)
-    assert_equal(1, @airport.number_of_passengers())
-  end  
-
-  def test_can_check_in_multiple_passengers
-    @airport.check_in_multiple_passengers(@passengers)
-    assert_equal(3, @airport.number_of_passengers())
   end
 
 
--- specs/flight_spec.rb
+++ specs/flight_spec.rb
@@ -1,11 +1,22 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../flight')
+require_relative('../passenger')
+require_relative('../airport')
+
 
 class FlightTest < Minitest::Test
 
   def setup
-    @flight = Flight.new(25, "Los Angeles", "Tracy")
+
+    @passenger1 = Passenger.new("David")
+    @passenger2 = Passenger.new("Pooh")
+    @passenger3 = Passenger.new("Max")
+    @passengers = [@passenger1, @passenger2, @passenger3]
+
+    @flight = Flight.new(25, "Los Angeles", [@passengers], "Tracy")
+
+
   end
 
   def test_flight_has_destination
@@ -17,7 +28,16 @@ class FlightTest < Minitest::Test
   end
   
   def test_flight_has_passenger
-    assert_equal("Tracy", @flight.passengers())
+    assert_equal("Tracy", @flight.passenger())
   end
 
+  def test_can_check_in_passenger
+    @flight.check_in_passenger(@passenger3)
+    assert_equal(2, @flight.number_of_passengers())
+  end  
+
+  def test_can_check_in_multiple_passengers
+    @flight.check_in_multiple_passengers(@passengers)
+    assert_equal(4, @flight.number_of_passengers())
+  end
 end
